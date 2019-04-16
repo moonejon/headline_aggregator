6280d87e70dd8612912e76ccbfc68e2c7207a118
Jonathan Mooney <moonejon@gmail.com>
2019-04-15 23:06:08 -0500

    added description and wired up scrape button

--- public/app.js
+++ public/app.js
@@ -1,12 +1,31 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
+
+$(document).ready(function () {
+
+  initiatePage();
+  $(document).on("click", "#scrape", function () { 
+    scrapeArticles();
+  })
 });
 
+  function initiatePage() {
+    $.getJSON("/articles", function (data) {
+      // For each one
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href='" + data[i].link + "'>" + data[i].link + "</a>" + "<br />" + data[i].description + "</p>");
+      }
+      console.log("world");
+    });
+  };
+
+function scrapeArticles() {
+
+    $.get("/scrape").then(function (data) {
+      console.log("hello");
+      initiatePage();
+    })
+}
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
